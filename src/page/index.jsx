067a91194cc0c9af320a654537e8c2e30c125988
Davi-html <davifernandes3674@gmail.com>
2024-01-26 16:39:35 -0300

    terminando a parte das info

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -7,6 +7,36 @@ export function Page(){
         <Container>
             <Header/>
             <Slider/>
+                <div className="informative-banners">
+                    <div className="frete">
+                        <img src="src/assets/shipping-circle.svg" alt="" />
+                        <div className="desc-info">
+                            <h3>Frete Gratís</h3>
+                            <p>Frete gratuito para todo Brasil!</p>
+                        </div>
+                    </div>
+                    <div className="preco">
+                        <img src="src/assets/promotions-circle.svg" alt="" />
+                        <div className="desc-info">
+                            <h3>Melhores preços</h3>
+                            <p>Economize comprando nossos produtos com até com até 30% de desconto!</p>
+                        </div>
+                    </div>
+                    <div className="suporte">
+                        <img src="src/assets/whatsapp-circle.svg" alt="" />
+                        <div className="desc-info">
+                            <h3>Precisa de suporte?</h3>
+                            <p>Obtenha um suporte rápido e eficaz, contate-nos no WhatsApp!</p>
+                        </div>
+                    </div>
+                    <div className="seguranca">
+                        <img src="src/assets/security-circle.svg" alt="" />
+                        <div className="desc-info">
+                            <h3>Segurança em primeiro lugar</h3>
+                            <p>Seus dados são processados com total segurança na hora da compra.</p>
+                        </div>
+                    </div>
+                </div>
         </Container>
     )
 }
--- src/page/styles.js
+++ src/page/styles.js
@@ -3,4 +3,18 @@ import styled from "styled-components";
 export const Container = styled.div`
     height: 100dvh;
     width: 100%;
+
+    .informative-banners {
+        height: 209px;
+        background-color: #D0D0D0;
+
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        align-items: center;
+        
+        gap: 15px;
+        padding: 20px;
+
+        font-family: 'Open Sans', sans-serif;
+    }
 `
